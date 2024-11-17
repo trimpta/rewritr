afb619ef28c63c9f5f5c4809b8d498d7691593db
Mehthab <mehthabunstoppable@gmail.com>
2024-11-17 18:21:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
-import pyperclip
+import win32api
+import pyautogui
 import time
+import pyperclip
+import os
 import requests
 import json
-import os
-import pyautogui
 
 prev = None
-while True:
-    time.sleep(0.1)
-    with pyautogui.hold('ctrl'):
-        pyautogui.press('c')
-    t =pyperclip.paste() 
-    
-    if t and t!= prev:
-        response = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{t}')
-        os.system('cls')
-        print(t)
-        if response.status_code == 200:
-            meanings = json.loads(response.text)[0]['meanings']
-            for i in meanings:
-                print('part of speech:',i['partOfSpeech'])
-                for j in i['definitions']:
-                    print('defintion:',j['definition'])
-                    if 'example' in j:
-                        print('example:',j['example'])
-                    print()
-                print()
 
-        pyperclip.copy('')
-        
-    prev = t
+state_left = win32api.GetKeyState(0x01)  # Left button down = 0 or 1. Button up = -127 or -128
+state_right = win32api.GetKeyState(0x02)  # Right button down = 0 or 1. Button up = -127 or -128
+
+while True:
+    try:
+        a = win32api.GetKeyState(0x01)
+
+        if a != state_left:  # Button state changed
+            state_left = a
+
+
+            if a>=0:
+                with pyautogui.hold('ctrl'):
+                    pyautogui.press('c')
+                t = pyperclip.paste()
+                
+                if t and t!= prev:
+                    response = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{t}')
+                    os.system('cls')
+                    print(t)
+                    if response.status_code == 200:
+                        meanings = json.loads(response.text)[0]['meanings']
+                        for i in meanings:
+                            print('part of speech:',i['partOfSpeech'])
+                            for j in i['definitions']:
+                                print('defintion:',j['definition'])
+                                if 'example' in j:
+                                    print('example:',j['example'])
+                                print()
+                            print()
+
+                    pyperclip.copy('')
+                    
+                prev = t
+    except:
+        pass
