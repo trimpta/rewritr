3b2043757cb599292e0487f91da90cd9be5b828c
Mehthab <mehthabunstoppable@gmail.com>
2024-11-23 15:11:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ prev = None
 state_left = win32api.GetKeyState(0x01)  # Left button down = 0 or 1. Button up = -127 or -128
 state_right = win32api.GetKeyState(0x02)  # Right button down = 0 or 1. Button up = -127 or -128
 
+
+
+
+from os import system
+
+system('mode con: cols=20 lines=49')
 while True:
     try:
         a = win32api.GetKeyState(0x01)
